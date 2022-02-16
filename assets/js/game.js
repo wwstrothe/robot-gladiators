85514146ae192fb0ebaf797564633e9753ccf96d
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-02-15 21:28:30 -0700

    made ability to play 1 round

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,43 @@
-function fight() {
-    window.alert("The fight has begun!");
-}
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-var playerName = window.prompt("What is your robot's name?")
+var enemyName = "Harold";
+var enemyHealth= 50;
+var enemyAttack = 12;
 
-console.log(playerName);
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ", " + enemyName + " now has " + enemyHealth + " health remaining"
+    );
+    
+    // check enemies health
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.")
+    }
 
-console.log("This logs a string, good for leaving yourself a message");
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ", " + playerName + " now has " + playerHealth + " health remaining"
+    );
 
-console.log(10 + 10);
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+    
+};
 
-console.log("Our robot's name is " + playerName)
-
-// fight();
+fight();
