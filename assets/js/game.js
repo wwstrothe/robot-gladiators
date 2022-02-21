50674c250d9de724b1aea0f37d3668125d85f723
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-02-21 09:57:00 -0700

    updated shop to make easier to navigate

--- assets/js/game.js
+++ assets/js/game.js
@@ -157,25 +157,22 @@ var endGame = function () {
 var shop = function () {
   console.log("entered the shop");
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
 
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   switch (shopOptionPrompt) {
-    case "REFILL": // new case
-    case "refill":
+    case 1:
       playerInfo.refillHealth();
       break;
-    case "UPGRADE": // new case
-    case "upgrade":
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case "LEAVE": // new case
-    case "leave":
+    case 3:
       window.alert("Leaving the store.");
       break;
     default:
       window.alert("You did not pick a valid option. Try again.");
-
       shop();
       break;
   }
