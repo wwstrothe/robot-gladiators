123fb400bfd3758390d72b123c67108fb37d52ea
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-02-21 21:24:37 -0700

    set up high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -143,16 +143,23 @@ var startGame = function () {
 // function to end game
 var endGame = function () {
   window.alert("The game has now ended. Let's see how you did!");
-
+  //check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0
+  }
   // if player still alive, player wins
-  if (playerInfo.health > 0) {
-    window.alert(
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(
       "Great job, you've survived the game! You now have a score of " +
         playerInfo.money +
         "."
     );
   } else {
-    window.alert("You've lost your robot in battle");
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
   //ask palyer if they'd like to play again
